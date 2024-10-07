4eae42ea1657f207136a3ec618fcd11385b1503a
mitch kamau <michchore@gmail.com>
2024-10-07 10:25:05 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let todo = document.getElementById('new-task-description').value
+    buildToDo(todo)
+    form.reset()
+  })
 });
+function buildToDo(todo) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  li.textContent = $(todo)
+  li.appendChild(btn)
+  console.log(li)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
